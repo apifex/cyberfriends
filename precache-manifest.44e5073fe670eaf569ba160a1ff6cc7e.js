2b4d0603ce8e5f4c742ae9a604b9e1bb3b62995f
Pawel Przebieda <pprzeb@gmail.com>
2020-03-30 20:51:09 +0200

    Updates

---
+++ precache-manifest.44e5073fe670eaf569ba160a1ff6cc7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e42b830d7f1b943a9baf1620fcf3d4",
+    "url": "/cyberfriends/index.html"
+  },
+  {
+    "revision": "75bbabd06efeb27b5f0b",
+    "url": "/cyberfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1c356469f26056845a4b",
+    "url": "/cyberfriends/static/css/main.7583647a.chunk.css"
+  },
+  {
+    "revision": "75bbabd06efeb27b5f0b",
+    "url": "/cyberfriends/static/js/2.f36469cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyberfriends/static/js/2.f36469cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c356469f26056845a4b",
+    "url": "/cyberfriends/static/js/main.f8b46457.chunk.js"
+  },
+  {
+    "revision": "a444b246a7e254af30f2",
+    "url": "/cyberfriends/static/js/runtime-main.19bf984d.js"
+  },
+  {
+    "revision": "a6c26b3b81ae67bb3b28ed1387e975e9",
+    "url": "/cyberfriends/static/media/Longstory.a6c26b3b.woff"
+  }
+]);
