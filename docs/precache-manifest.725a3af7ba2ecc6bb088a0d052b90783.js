a46237f7d0390472962b0cf3ffe390b615edd43b
Pawel Przebieda <pprzeb@gmail.com>
2020-03-30 19:29:15 +0200

    test2

---
+++ docs/precache-manifest.725a3af7ba2ecc6bb088a0d052b90783.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bfde63d4d79bea2f04b5d5d9c4da78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bbabd06efeb27b5f0b",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "10c881b2850a45a918a9",
+    "url": "/static/css/main.e8834c41.chunk.css"
+  },
+  {
+    "revision": "75bbabd06efeb27b5f0b",
+    "url": "/static/js/2.f36469cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f36469cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c881b2850a45a918a9",
+    "url": "/static/js/main.f8b46457.chunk.js"
+  },
+  {
+    "revision": "0d54bbeb267ac93a9e1f",
+    "url": "/static/js/runtime-main.26fd3c99.js"
+  },
+  {
+    "revision": "a6c26b3b81ae67bb3b28ed1387e975e9",
+    "url": "/static/media/Longstory.a6c26b3b.woff"
+  }
+]);
